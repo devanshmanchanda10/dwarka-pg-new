25fce9db1bf5297553dd0263381ed3e8718e6808
devanshmanchanda10 <manchandadevansh10@gmail.com>
2024-08-07 16:10:07 +0530

    first commit

---
+++ src/routes/singlePage/singlePage.jsx
@@ -0,0 +1,124 @@
+import "./singlePage.scss";
+import Slider from "../../components/slider/Slider";
+import Map from "../../components/map/Map";
+import { postsData, userData } from "../../lib/dummydata";
+import { useParams } from "react-router-dom";
+
+function SinglePage() {
+  const { id } = useParams(); // Assuming you are using React Router to pass the post ID via URL params
+  const post = postsData.find(post => post.id === parseInt(id, 10));
+
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  return (
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={post.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{post.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="" />
+                  <span>{post.address}</span>
+                </div>
+                <div className="price">$ {post.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">{post.description}</div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper">
+          <p className="title">General</p>
+          <div className="listVertical">
+            <div className="feature">
+              <img src="/utility.png" alt="" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>Renter is responsible</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Pet Policy</span>
+                <p>Pets Allowed</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Property Fees</span>
+                <p>Must have 3x the rent in total household income</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">Sizes</p>
+          <div className="sizes">
+            <div className="size">
+              <img src="/size.png" alt="" />
+              <span>{post.size} sqft</span>
+            </div>
+            <div className="size">
+              <img src="/bed.png" alt="" />
+              <span>{post.bedRooms} beds</span>
+            </div>
+            <div className="size">
+              <img src="/bath.png" alt="" />
+              <span>{post.bathroom} bathroom</span>
+            </div>
+          </div>
+          <p className="title">Nearby Places</p>
+          <div className="listHorizontal">
+            <div className="feature">
+              <img src="/school.png" alt="" />
+              <div className="featureText">
+                <span>School</span>
+                <p>{post.school}</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/bus.png" alt="" />
+              <div className="featureText">
+                <span>Bus Stop</span>
+                <p>{post.bus}</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/restaurant.png" alt="" />
+              <div className="featureText">
+                <span>Restaurant</span>
+                <p>{post.restaurant}</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">Location</p>
+          <div className="mapContainer">
+            <Map items={[post]} />
+          </div>
+          <div className="buttons">
+            <button>
+              <img src="/chat.png" alt="" />
+              Send a Message
+            </button>
+            <button>
+              <img src="/save.png" alt="" />
+              Save the Place
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SinglePage;
